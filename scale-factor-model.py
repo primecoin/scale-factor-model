06c24c7b7d67285639dd011c66cceabc248be684
Jubileus <Jubileus_ryz@protonmail.com>
2022-03-19 19:14:30 -0700

    update

--- scale-factor-model.py
+++ scale-factor-model.py
@@ -6,18 +6,15 @@ if __name__=="__main__":
     Count = {}
     for i in range(0, 15) :
         Count[i] = 0
-    for i in range(4651555, 4651595): #4651595
+    for i in range(4651555, 4651565): #4651595
         response = requests.get(url + str(i), headers = headers).json()
         difficulty = response["result"]["difficulty"]
         primeChain = response["result"]["primechain"]
         chainLength = int(primeChain[3:5], 16)
         print(chainLength)
         print(difficulty)
-        if (chainLength == 10) :
-            if difficulty >= 10 and difficulty < 11:
-                Count[10] += 1 / (11 - difficulty)
-            else:
-                Count[10] += 1
+        if (chainLength == int(difficulty)) :
+            Count[10] += 1 / (int(difficulty) + 1 - difficulty)
         else:
             Count[chainLength] += 1
     print(Count)
