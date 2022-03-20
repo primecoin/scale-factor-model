452cdd539b7bb31a1963883c81a4387f848727f2
Jubileus <Jubileus_ryz@protonmail.com>
2022-03-19 19:08:37 -0700

    update

--- scale-factor-model.py
+++ scale-factor-model.py
@@ -3,30 +3,22 @@ import requests
 if __name__=="__main__":
     headers = {"content-type": "application/json"}
     url = "https://explorer.primecoin.net/api/rest/blockchain/getblock/"
-    cnt_10 = 0
-    cnt_11 = 0
-    cnt_12 = 0
-    cnt_13 = 0
-    for i in range(4650595, 4651595): #4651595
-	    response = requests.get(url + str(i), headers = headers).json()
-	    difficulty = response["result"]["difficulty"]
-	    primeChain = response["result"]["primechain"]
-	    chainLens = int(primeChain[3:5], 16)
-	    print(chainLens)
-	    print(difficulty)
-	    if chainLens == 13 :
-	        cnt_13 += 1
-	    elif chainLens == 12 :
-	        cnt_12 += 1
-	    elif chainLens == 11 :
-	        cnt_11 += 1
-	    elif chainLens == 10 :
-                if difficulty >= 10 and difficulty < 11:
-                    cnt_10 += 1 / (11 - difficulty)
-                else :
-                    cnt_10 += 1 
-    print(cnt_10)
-    print(cnt_11)    
-    print(cnt_12)
-    print(cnt_13)
+    Count = {}
+    for i in range(0, 15) :
+        Count[i] = 0
+    for i in range(4651555, 4651595): #4651595
+        response = requests.get(url + str(i), headers = headers).json()
+        difficulty = response["result"]["difficulty"]
+        primeChain = response["result"]["primechain"]
+        chainLength = int(primeChain[3:5], 16)
+        print(chainLength)
+        print(difficulty)
+        if (chainLength == 10) :
+            if difficulty >= 10 and difficulty < 11:
+                Count[10] += 1 / (11 - difficulty)
+            else:
+                Count[10] += 1
+        else:
+            Count[chainLength] += 1
+    print(Count)
     
